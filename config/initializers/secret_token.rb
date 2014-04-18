49dd60ac77f3eb2b14a047001b6bca8a1edf7f23
Tim Pratt <tim.pratt@ilifeone.com>
2014-04-17 21:07:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ILifeOne::Application.config.secret_key_base = '74e73cf8cc3c5d0c1b5e72704a2445d7b44438c59c541fbf4f03019aaeb8c6b0f5f112a761e948d88b64a17eb1ad25ce37ed1c36ac91500911fc394f41e27fc2'
